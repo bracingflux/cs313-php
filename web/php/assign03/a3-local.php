bbe7681aa748735bc379e81f2e1d50a9b8398444
Eli Andrew <and16088@byui.edu>
2018-05-22 12:48:37 -0600

    Added changes to database query

--- web/php/assign03/a3-local.php
+++ web/php/assign03/a3-local.php
@@ -4,7 +4,28 @@
 </head>
 <body>
 	<?php 
-		try
+		$dbUrl = getenv('DATABASE_URL');
+		if (empty($dbUrl)) {
+			 $dbUrl = "postgres://postgres:Linus98!@localhost:5432/postgres";
+		}
+		echo "$dbUrl<br>";
+		// $dbopts = parse_url($dbUrl);
+
+		// print "<p>$dbUrl</p>\n\n";
+		$dbHost = "localhost";
+		$dbPort = "5432";
+		$dbName = "postgres";
+		$dbUser = "postgres";
+		$dbPassword = "Linus98!";
+
+		try {
+		 $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+		}
+		catch (PDOException $ex) {
+			print("<p>error:" . $ex->getMessage() . "</p><br>");
+			die();
+		}
+		/*try
 		{
 		  $user = 'postgres';
 		  $password = 'Linus98!';
@@ -19,7 +40,7 @@
 		{
 		  echo 'Error!: ' . $ex->getMessage() . " nope..";
 		  die();
-		}
+		}*/
 		
 	?>
 </body>
