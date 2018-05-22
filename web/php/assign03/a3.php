fdd61adb7d3345858b9bfd10d574ee1ba68ddadc
Eli Andrew <and16088@byui.edu>
2018-05-22 10:06:16 -0600

    Another change

--- web/php/assign03/a3.php
+++ web/php/assign03/a3.php
@@ -32,11 +32,11 @@
 			$db = new PDO("pgsql:host=$mHost;port=$mPort;dbname=$dbName", $mUser, $mPass);
 			echo "<p>db set</p>";
 			$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-			/*foreach ($db->query('SELECT name, description FROM root_beers') as $row) {
+			foreach ($db->query('SELECT name, description FROM root_beers') as $row) {
 				echo "Root Beer name: " . $row['name'];
 				echo "Description: " . $row['description'];
 				echo "<br>";
-			}*/
+			}
 			echo "<p>Items should have printed</p>";
 		}
 		catch(PDOException $ex) {
