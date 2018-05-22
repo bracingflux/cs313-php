df40130e7d6ea0fb3a16ae5485476c6a93965802
Eli Andrew <and16088@byui.edu>
2018-05-22 12:50:55 -0600

    another change.

--- web/php/assign03/a3.php
+++ web/php/assign03/a3.php
@@ -3,26 +3,26 @@
 <head>	
 </head>
 <body>
+	<p>Hello!</p>
 	<?php 
-		/*$dbUrl = getenv('DATABASE_URL');		
+		$dbUrl = getenv('DATABASE_URL');		
 		if (empty($dbUrl)) {
 			echo "it's empty";
 			$dbUrl = "postgres://hmufjxaoraveoi:8004b598443a41c86155a553beab2246b64842706de2c90402b37824e1889767@ec2-23-23-130-158.compute-1.amazonaws.com:5432/d7llnf8glafh95";
 		}
 		echo "dbUrl: $dbUrl";
-		$dbopts = parse_url($dbUrl);*/
+		$dbopts = parse_url($dbUrl);
 
 		$mHost = "ec2-23-23-130-158.compute-1.amazonaws.com";
 		$mPort = "5432";
 		$mUser = "hmufjxaoraveoi";
 		$mPass = "8004b598443a41c86155a553beab2246b64842706de2c90402b37824e1889767";
-		$dbName = "d7llnf8glafh95";
 
-		// $dbHost = $dbopts["ec2-23-23-130-158.compute-1.amazonaws.com"];
-		// $dbPort = $dbopts["5432"];
-		// $dbUser = $dbopts["hmufjxaoraveoi"];
-		// $dbPassword = $dbopts["8004b598443a41c86155a553beab2246b64842706de2c90402b37824e1889767"];
-		// $dbName = ltrim($dbopts["path"],'/');
+		$dbHost = $dbopts["ec2-23-23-130-158.compute-1.amazonaws.com"];
+		$dbPort = $dbopts["5432"];
+		$dbUser = $dbopts["hmufjxaoraveoi"];
+		$dbPassword = $dbopts["8004b598443a41c86155a553beab2246b64842706de2c90402b37824e1889767"];
+		$dbName = ltrim($dbopts["path"],'/');
 		echo "<p>Working..</p>";
 		print "<p>pgsql:host=$mHost;port=$mPort;dbname=$dbName User: $mUser Pass: $mPass</p>\n\n";
 		try
@@ -35,7 +35,7 @@
 			foreach ($db->query('SELECT name, description FROM root_beers') as $row) {
 				echo "Name: " . $row['name'] . "<br>";
 				echo "Description: " . $row['description'] . "<br>";
-				echo "Type: " $row["type"] . "<br>" . "Brand: " $row["brand"] . "<br>"; 
+				echo "Type: " $row["type"] . "<br>" . "Brand: " $row["brand"] . "<br>" 
 			}
 			echo "<p>Items should have printed</p>";
 		}
