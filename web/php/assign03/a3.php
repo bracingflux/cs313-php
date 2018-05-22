18b3fcba1763a8447baf9e76629b6c67509149f1
Eli Andrew <and16088@byui.edu>
2018-05-22 00:14:31 -0600

    minor adjustments

--- web/php/assign03/a3.php
+++ web/php/assign03/a3.php
@@ -5,30 +5,23 @@
 <body>
 	<p>Hello!</p>
 	<?php 
-		try
-		{
-			$dbUrl = getenv('postgres://hmufjxaoraveoi:8004b598443a41c86155a553beab2246b64842706de2c90402b37824e1889767@ec2-23-23-130-158.compute-1.amazonaws.com:5432/d7llnf8glafh95');
+		$dbUrl = getenv('postgres://hmufjxaoraveoi:8004b598443a41c86155a553beab2246b64842706de2c90402b37824e1889767@ec2-23-23-130-158.compute-1.amazonaws.com:5432/d7llnf8glafh95');
 
-			$dbopts = parse_url($dbUrl);
+		$dbopts = parse_url($dbUrl);
 
-			$dbHost = $dbopts["ec2-23-23-130-158.compute-1.amazonaws.com"];
-			$dbPort = $dbopts["5432"];
-			$dbUser = $dbopts["hmufjxaoraveoi"];
-			$dbPassword = $dbopts["8004b598443a41c86155a553beab2246b64842706de2c90402b37824e1889767"];
-			$dbName = ltrim($dbopts["d7llnf8glafh95"],'/');
+		$dbHost = $dbopts["ec2-23-23-130-158.compute-1.amazonaws.com"];
+		$dbPort = $dbopts["5432"];
+		$dbUser = $dbopts["hmufjxaoraveoi"];
+		$dbPassword = $dbopts["8004b598443a41c86155a553beab2246b64842706de2c90402b37824e1889767"];
+		$dbName = ltrim($dbopts["d7llnf8glafh95"],'/');
 
-			$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+		$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
-			$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-			foreach ($db->query('SELECT name, description FROM root_beers') as $row) {
-				echo "Root Beer name: " . $row['name'];
-				echo "Description: " . $row['description'];
-				echo "<br>";
-			}
-		}
-		catch (PDOExecption $ex) {
-			echo "Error!: " . $ex->getMessage();
-			die();
+		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		foreach ($db->query('SELECT name, description FROM root_beers') as $row) {
+			echo "Root Beer name: " . $row['name'];
+			echo "Description: " . $row['description'];
+			echo "<br>";
 		}
 		
 	?>
