26f404ac85d23ce1decb740ef620a4b592c5bbce
Eli Andrew <and16088@byui.edu>
2018-05-22 15:56:23 -0600

    additional team activity changes.

--- web/php/team05/scriptures.php
+++ web/php/team05/scriptures.php
@@ -9,10 +9,8 @@
    <?php 
       $dbUrl = getenv('DATABASE_URL');    
       if (empty($dbUrl)) {
-         echo "it's empty";
          $dbUrl = "postgres://hmufjxaoraveoi:8004b598443a41c86155a553beab2246b64842706de2c90402b37824e1889767@ec2-23-23-130-158.compute-1.amazonaws.com:5432/d7llnf8glafh95";
       }
-      echo "dbUrl: $dbUrl";
       $dbopts = parse_url($dbUrl);
 
       $mHost = "ec2-23-23-130-158.compute-1.amazonaws.com";
@@ -25,20 +23,15 @@
       $dbUser = $dbopts["hmufjxaoraveoi"];
       $dbPassword = $dbopts["8004b598443a41c86155a553beab2246b64842706de2c90402b37824e1889767"];
       $dbName = ltrim($dbopts["path"],'/');
-      echo "<p>Working..</p>";
-      print "<p>pgsql:host=$mHost;port=$mPort;dbname=$dbName User: $mUser Pass: $mPass</p>\n\n";
       try
       {
-         // $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
          $db = new PDO("pgsql:host=$mHost;port=$mPort;dbname=$dbName", $mUser, $mPass);
-         // echo "<p>db set</p>";
          $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
          foreach ($db->query('SELECT book, chapter, verse, content FROM scripture') as $row) {
-            echo "<p>" . $row['book'] . " " . $row['chapter'] . ":" . $row['verse'] . " - \"" . $row['content'] . "\"</p><br>";
+            echo "<p><strong>" . $row['book'] . " " . $row['chapter'] . ":" . $row['verse'] . "<strong> - \"" . $row['content'] . "\"</p><br>";
          }
-         echo "<p>Items should have printed</p>";
       }
       catch(PDOException $ex) {
          echo "<p>Error: " . $ex->getMessage() . "</p><br>";
