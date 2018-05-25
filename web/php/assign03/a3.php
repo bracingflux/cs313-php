d0beccdd46578f49877eb384f631ad68318b568c
Eli Andrew <and16088@byui.edu>
2018-05-24 20:17:20 -0600

    added env variables

--- web/php/assign03/a3.php
+++ web/php/assign03/a3.php
@@ -7,31 +7,25 @@
 <body>
 	<p>Hello! This is a database query page!</p>
 	<?php 
-		$dbUrl = getenv('DATABASE_URL');		
+		$dbUrl = getenv('HEROKU_POSTGRESQL_AMBER_URL');		
 		if (empty($dbUrl)) {
 			echo "it's empty";
 			$dbUrl = "postgres://hmufjxaoraveoi:8004b598443a41c86155a553beab2246b64842706de2c90402b37824e1889767@ec2-23-23-130-158.compute-1.amazonaws.com:5432/d7llnf8glafh95";
 		}
-		echo "dbUrl: $dbUrl";
-		$dbopts = parse_url($dbUrl);
-
-		$mHost = "ec2-23-23-130-158.compute-1.amazonaws.com";
-		$mPort = "5432";
-		$mUser = "hmufjxaoraveoi";
-		$mPass = "8004b598443a41c86155a553beab2246b64842706de2c90402b37824e1889767";
-
-		$dbHost = $dbopts["ec2-23-23-130-158.compute-1.amazonaws.com"];
-		$dbPort = $dbopts["5432"];
-		$dbUser = $dbopts["hmufjxaoraveoi"];
-		$dbPassword = $dbopts["8004b598443a41c86155a553beab2246b64842706de2c90402b37824e1889767"];
+		$dbopts = parse_url($dbUrl);		
+		$dbHost = $dbopts["host"];
+		$dbPort = $dbopts["port"];
+		$dbUser = $dbopts["user"];
+		$dbPassword = $dbopts["pass"];
 		$dbName = ltrim($dbopts["path"],'/');
 		echo "<p>Working..</p>";
+
 		print "<p>pgsql:host=$mHost;port=$mPort;dbname=$dbName User: $mUser Pass: $mPass</p>\n\n";
 		try
 		{
 			// $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
-			$db = new PDO("pgsql:host=$mHost;port=$mPort;dbname=$dbName", $mUser, $mPass);
+     		$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);			
 			echo "<p>db set</p>";
 			$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			foreach ($db->query('SELECT name, description FROM rb_test') as $row) {
