7309c83456c8f94cba0124d011d7868e5b08c468
Eli Andrew <and16088@byui.edu>
2018-05-25 15:50:53 -0600

    more css styles

--- web/php/assign03/a3.php
+++ web/php/assign03/a3.php
@@ -2,10 +2,13 @@
 <html lang="en-US">
 <head>
 	<meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">	
 	<link rel="stylesheet" type="text/css" href="../../css/a3.css">
 	<title>Root Beer Revelry</title>	
 </head>
 <body>
+	<a href="https://www.facebook.com/Root-Beer-Revelry-181821778540286/" target="_blank" class="fa fa-facebook" id="shadow"></a>
+	<a href="https://twitter.com/rootbeerrevelry?lang=en" class="fa fa-twitter" target="_blank" id="shadow"></a>
 	<div id="banner">
 		<img src="../../photos/revelry.jpg" class="center" alt="Root Beer Revelry banner">
 		<p class="def">
