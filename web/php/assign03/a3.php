1c4e30f601b8bc914eb8170fc122c2cb441cc3c7
Eli Andrew <and16088@byui.edu>
2018-05-24 20:24:21 -0600

    made load_db file for loading database externally

--- web/php/assign03/a3.php
+++ web/php/assign03/a3.php
@@ -6,8 +6,9 @@
 </head>
 <body>
 	<p>Hello! This is a database query page!</p>
-	<?php 
-		$dbUrl = getenv('HEROKU_POSTGRESQL_AMBER_URL');		
+	<?php
+	require('load_db.php'); 
+		/*$dbUrl = getenv('HEROKU_POSTGRESQL_AMBER_URL');		
 		if (empty($dbUrl)) {
 			$dbUrl = "postgres://hmufjxaoraveoi:8004b598443a41c86155a553beab2246b64842706de2c90402b37824e1889767@ec2-23-23-130-158.compute-1.amazonaws.com:5432/d7llnf8glafh95";
 		}
@@ -21,15 +22,15 @@
 		try
 		{
      		$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);			
-			$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);*/
 			foreach ($db->query('SELECT name, description FROM rb_test') as $row) {
 				echo "Root Beer name: " . $row['name'] . "<br>";
 			}
-		}
+		/*}
 		catch(PDOException $ex) {
 			echo "<p>Error: " . $ex->getMessage() . "</p><br>";
 			die();
-		}
+		}*/
 
 		
 		
