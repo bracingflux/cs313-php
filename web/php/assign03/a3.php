4a4f0f3a2a1c568a262d43cd8f99d0895e772860
Eli Andrew <and16088@byui.edu>
2018-05-24 20:37:15 -0600

    Added css file and more styling

--- web/php/assign03/a3.php
+++ web/php/assign03/a3.php
@@ -2,38 +2,17 @@
 <html lang="en-US">
 <head>
 	<meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="../../css/a3.css">
 	<title>Database Query</title>	
 </head>
 <body>
 	<p>Hello! This is a database query page!</p>
 	<?php
-	require('load_db.php'); 
-		/*$dbUrl = getenv('HEROKU_POSTGRESQL_AMBER_URL');		
-		if (empty($dbUrl)) {
-			$dbUrl = "postgres://hmufjxaoraveoi:8004b598443a41c86155a553beab2246b64842706de2c90402b37824e1889767@ec2-23-23-130-158.compute-1.amazonaws.com:5432/d7llnf8glafh95";
-		}
-		$dbopts = parse_url($dbUrl);		
-		$dbHost = $dbopts["host"];
-		$dbPort = $dbopts["port"];
-		$dbUser = $dbopts["user"];
-		$dbPassword = $dbopts["pass"];
-		$dbName = ltrim($dbopts["path"],'/');
+	require('load_db.php');
 
-		try
-		{
-     		$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);			
-			$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);*/
-			foreach ($db->query('SELECT name, description FROM rb_test') as $row) {
-				echo "Root Beer name: " . $row['name'] . "<br>";
-			}
-		/*}
-		catch(PDOException $ex) {
-			echo "<p>Error: " . $ex->getMessage() . "</p><br>";
-			die();
-		}*/
-
-		
-		
+	foreach ($db->query('SELECT name, description FROM rb_test') as $row) {
+		echo "<div class='rb_item'><p>" . $row['name'] . "<div>";
+	}		
 	?>
 </body>
 </html>
