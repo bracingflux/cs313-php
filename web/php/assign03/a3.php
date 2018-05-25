9da52b2f74b2896ab1440b2d4be46797fd65d43a
Eli Andrew <and16088@byui.edu>
2018-05-25 16:18:23 -0600

    added pop-up window and a3.js file

--- web/php/assign03/a3.php
+++ web/php/assign03/a3.php
@@ -2,8 +2,13 @@
 <html lang="en-US">
 <head>
 	<meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">	
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+	<link rel="stylesheet" href="/resources/demos/style.css">
+	<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 	<link rel="stylesheet" type="text/css" href="../../css/a3.css">
+	<script type="text/javascript" src="../../javascript/a3.js"></script>
 	<title>Root Beer Revelry</title>	
 </head>
 <body>
@@ -20,7 +25,25 @@
 			<img id="rbPhoto" src="../../photos/rbs/Abita Root Beer.png" alt="Abita Root Beer">
 			<p id="rbName">Abita Root Beer</p>
 	</div> -->
+	<div id="dialog-form" title="Create new user">
+	  <p class="validateTips">All form fields are required.</p>
+	 
+	  <form>
+	    <fieldset>
+	      <label for="name">Name</label>
+	      <input type="text" name="name" id="name" value="Jane Smith" class="text ui-widget-content ui-corner-all">
+	      <label for="email">Email</label>
+	      <input type="text" name="email" id="email" value="jane@smith.com" class="text ui-widget-content ui-corner-all">
+	      <label for="password">Password</label>
+	      <input type="password" name="password" id="password" value="xxxxxxx" class="text ui-widget-content ui-corner-all">
+	 
+	      <!-- Allow form submission with keyboard without duplicating the dialog button -->
+	      <input type="submit" tabindex="-1" style="position:absolute; top:-1000px">
+	    </fieldset>
+	  </form>
+	</div>
 
+	<button id="create-user">Create new user</button>
 	<?php
 	require('load_db.php');
 
