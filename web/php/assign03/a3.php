3c9a30048b5648b0e5c72662840fc82b20d5ae99
Eli Andrew <and16088@byui.edu>
2018-05-24 20:55:10 -0600

    added revelry banner

--- web/php/assign03/a3.php
+++ web/php/assign03/a3.php
@@ -6,7 +6,13 @@
 	<title>Database Query</title>	
 </head>
 <body>
-	<p>Hello! This is a database query page!</p>
+	<div style="border-bottom: 5px solid #c3a2a2; padding-bottom: .5%; border-radius: 5px;">
+		<img src="../../photos/revelry.jpg" class="center" alt="Root Beer Revelry banner" style="height: 7.5%; width: 39.62%;">
+		<p class="def" style="margin-left: 52%;">
+			rev·el·ry \'re-vəl-rē\<br> n. pl. rev·el·ries<br> Boisterous merrymaking.
+		</p>
+	</div>
+	
 	<?php
 	require('load_db.php');
 
