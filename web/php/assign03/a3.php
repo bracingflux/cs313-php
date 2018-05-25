50bb540e892887ee72cf93e57e2881c51b2ba7cb
Eli Andrew <and16088@byui.edu>
2018-05-25 14:46:33 -0600

    css styles

--- web/php/assign03/a3.php
+++ web/php/assign03/a3.php
@@ -17,7 +17,7 @@
 	require('load_db.php');
 
 	foreach ($db->query('SELECT name FROM root_beers') as $row) {
-		echo "<div class='row'><img src=\"../../photos/rbs/" . $row['name'] . ".png\" class='center_photo' alt='" . $row['name'] . "'>";
+		echo "<div class='row'><div class='rb_photo'><img src=\"../../photos/rbs/" . $row['name'] . ".png\" class='center_photo' alt='" . $row['name'] . "'></div>";
 		echo "<p class='white'>" . $row['name'] ."</p></div>";		
 	}		
 	?>
