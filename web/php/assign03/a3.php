74fdd1c3261264dd2b1b75f6ba34f4980e66fb9b
Eli Andrew <and16088@byui.edu>
2018-05-24 21:47:47 -0600

    added photos

--- web/php/assign03/a3.php
+++ web/php/assign03/a3.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" type="text/css" href="../../css/a3.css">
-	<title>Database Query</title>	
+	<title>Root Beer Revelry</title>	
 </head>
 <body>
 	<div id="banner">
@@ -16,7 +16,8 @@
 	<?php
 	require('load_db.php');
 
-	foreach ($db->query('SELECT name FROM rb_test') as $row) {
+	foreach ($db->query('SELECT name FROM root_beers') as $row) {
+		echo "<img src='../../photos/rbs/" . $row['name'] . " class='center' alt='" . $row['name'] . "'>";
 		echo "<div class='rb_item'><p class='white'>" . $row['name'] ."</p></div>";
 	}		
 	?>
