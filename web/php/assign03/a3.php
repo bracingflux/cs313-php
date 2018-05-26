ccf9b09d8ace3834aef18799810d5a1ff7884b03
Eli Andrew <and16088@byui.edu>
2018-05-26 13:26:17 -0600

    more css changes

--- web/php/assign03/a3.php
+++ web/php/assign03/a3.php
@@ -12,7 +12,7 @@
 	<title>Root Beer Revelry</title>	
 </head>
 <body>
-	<?php $rb = ""; ?>
+	
 	<a href="https://www.facebook.com/Root-Beer-Revelry-181821778540286/" target="_blank" class="fa fa-facebook" id="shadow"></a>
 	<a href="https://twitter.com/rootbeerrevelry?lang=en" class="fa fa-twitter" target="_blank" id="shadow"></a>
 	<div id="banner">
