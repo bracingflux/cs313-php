47b5c8ca1977443a31b072d70868c56c53f0a920
Eli Andrew <and16088@byui.edu>
2018-05-26 13:48:23 -0600

    More php changes

--- web/php/assign03/rb_data.php
+++ web/php/assign03/rb_data.php
@@ -3,16 +3,26 @@
 
 	if (isset($_POST)) {
 		$rb = "";
+		$id = 0;
 		foreach ($_POST as $key => $value) {
 	  		$rb = $value;
 	  	}
 
-	  	$stmt = $db->prepare('SELECT name, description, type, brand FROM root_beers WHERE name=:name');
+	  	$stmt = $db->prepare('SELECT name, description, type, brand, id FROM root_beers WHERE name=:name');
 		$stmt->execute(array(':name' => $rb));
 		$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 		foreach ($rows as $row) {
 			echo "Name: " . $row['name'] . "\n\nDescription: " . $row['description'] . "\n\nType: " . $row['type']
 			. "\n\nBrand: " . $row['brand'];
+			$id = $row['id'];
+		}
+
+		$stmt2 = $db->prepare('SELECT "text", "timestamp" FROM comments WHERE root_beer_id=:id');
+		$stmt2->execute(array(':id' => $id));
+		$comment_rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+		foreach ($comment_rows as $row) {
+			echo "Comments:\n\nTime: " . $row['timestamp'] . "\n\nComment: " . $row['text'];
 		}
 	}
 	else {
