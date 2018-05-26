23a456b5763f9402579e2892071a1ffd4e236871
Eli Andrew <and16088@byui.edu>
2018-05-26 11:10:38 -0600

    more javascript changes

--- web/php/assign03/rb_data.php
+++ web/php/assign03/rb_data.php
@@ -1,8 +1,11 @@
 <?php 
-	/*if (isset(_POST)) {*/
-		echo "var_dump($_POST)";
+	if (isset($_POST)) {
+
 		foreach ($_POST as $key => $value) {
 	  		echo "<h1>Key: $key Value: $value<h1>";
 	  	}
-	// }
+	}
+	else {
+		echo "POST not set..";
+	}
  ?>
