6e71f4e17607de6541b7d6e8c5f247569098ebcf
Eli Andrew <and16088@byui.edu>
2018-05-26 10:20:38 -0600

    more javascript changes

--- web/javascript/a3.js
+++ web/javascript/a3.js
@@ -89,56 +89,25 @@
     });
   } );
 
-  $("#rbForm").submit(function(event){
 
-    // Prevent default posting of form - put here to work in case of errors
-    event.preventDefault();
 
-    // Abort any pending request
-    if (request) {
-        request.abort();
-    }
-    // setup some local variables
-    var $form = $(this);
+ $(function () {
 
-    // Let's select and cache all the fields
-    var $inputs = $form.find("input, select, button, textarea");
+  $('#rbForm').on('submit', function (e) {
 
-    // Serialize the data in the form
-    var serializedData = $form.serialize();
+    e.preventDefault();
 
-    // Let's disable the inputs for the duration of the Ajax request.
-    // Note: we disable elements AFTER the form data has been serialized.
-    // Disabled form elements will not be serialized.
-    $inputs.prop("disabled", true);
-
-    // Fire off the request to /form.php
-    request = $.ajax({
-        /*url: "a3.php",*/
-        type: "post",
-        data: serializedData
+    $.ajax({
+      type: 'post',
+      url: 'a3.php',
+      data: $('#rbForm').serialize(),
+      success: function () {
+        alert('form was submitted');
+      }
     });
 
-    // Callback handler that will be called on success
-    request.done(function (response, textStatus, jqXHR){
-        // Log a message to the console
-        console.log("Hooray, it worked!");
-    });
-
-    // Callback handler that will be called on failure
-    request.fail(function (jqXHR, textStatus, errorThrown){
-        // Log the error to the console
-        console.error(
-            "The following error occurred: "+
-            textStatus, errorThrown
-        );
-    });
-
-    // Callback handler that will be called regardless
-    // if the request failed or succeeded
-    request.always(function () {
-        // Reenable the inputs
-        $inputs.prop("disabled", false);
-    });
+  });
 
 });
+
+   
