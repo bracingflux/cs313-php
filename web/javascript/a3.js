86e42532f80b458cac82ecb6ba29245435ace714
Eli Andrew <and16088@byui.edu>
2018-05-26 12:21:00 -0600

    more javascript changes

--- web/javascript/a3.js
+++ web/javascript/a3.js
@@ -8,21 +8,17 @@
       width: 500,
       modal: true,
       buttons: {
-        // "Create an account": addUser,
         Cancel: function() {
           dialog.dialog( "close" );
         }
       },
       close: function() {
         $('#loaded_rb').text("");
-        // form[ 0 ].reset();
-        // allFields.removeClass( "ui-state-error" );
       }
     });
  
     form = dialog.find( "form" ).on( "submit", function( event ) {
       event.preventDefault();
-      // addUser();
     });
  
     $( ".create-user" ).button().on( "click", function() {      
@@ -40,7 +36,6 @@
     var $form = $(this);
     var $inputs = $form.find("input, select, button, textarea");
     var serializedData = $form.serialize();
-    // alert(serializedData);
     
     $.ajax({  
       type: 'post',
@@ -48,8 +43,7 @@
       data: serializedData,
       success: function (response) {
         $('#loaded_rb').text(response);
-        $('#checkoutItems').load(document.URL + ' #dialog-form');
-        // alert('form was submitted' + " " + response);        
+        // $('#checkoutItems').load(document.URL + ' #dialog-form');
       }
     });
 
