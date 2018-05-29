8448f194bd8f5a5a11d1c9f20e53069e2fce4d6b
Eli Andrew <and16088@byui.edu>
2018-05-29 16:33:06 -0600

    sdf

--- web/php/team05/update_db.php
+++ web/php/team05/update_db.php
@@ -9,6 +9,8 @@
    $verse = $_POST['verse'];
    $content = $_POST['content'];
 
+   echo "VARIABLES SET: $book $chapter $verse $content <br>";
+
    $stmt = $db->prepare('INSERT INTO scripture (book, chapter, verse, content) VALUES (:book, :chapter, :verse, :content)');
     $stmt->bindValue(':book', $book, PDO::PARAM_STR);
     $stmt->bindValue(':chapter', $chapter, PDO::PARAM_INT);
@@ -18,9 +20,11 @@
          // $stmt->execute(array(':book' => $book));
          $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+         echo "Before<br>";
          foreach ($rows as $row) {
-            echo "$row<br>";
-          } 
+            echo "Row: $row<br>";
+          }
+          echo "After<br>"; 
 
    // $stmt = $db->prepare('INSERT INTO scripture (book, chapter, verse, content) VALUES (book, chapter, verse, content) WHERE book=:book AND chapter=:chapter AND verse=:verse AND content=:content');
  ?>
