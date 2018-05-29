142471a783331d8e8105f77964ab3e5d5cbbc031
Eli Andrew <and16088@byui.edu>
2018-05-29 16:08:07 -0600

    sdf

--- web/php/team05/add_scripture.php
+++ web/php/team05/add_scripture.php
@@ -12,21 +12,21 @@
    <form action="update_db.php" method="POST">
      <div class="form-group">
        <label for="exampleInputEmail1">Book</label>
-       <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Alma">
+       <input type="text" name="book" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Alma">
        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
     </div>
 
     <div class="form-group">
        <label for="exampleInputPassword1">Chapter</label>
-       <input type="text" class="form-control" id="exampleInputPassword1" placeholder="32">
+       <input type="text" name="chapter" class="form-control" id="exampleInputPassword1" placeholder="32">
     </div>
     <div class="form-group">
        <label for="exampleInputPassword1">Verse</label>
-       <input type="text" class="form-control" id="exampleInputPassword1" placeholder="31">
+       <input type="text" name="verse" class="form-control" id="exampleInputPassword1" placeholder="31">
     </div>
     <div class="form-group">
        <label for="exampleInputPassword1">Content</label>
-       <textarea style="width: 600px;" placeholder="But behold, if ye will awake and arouse your faculties, even to an experiment upon my words, and exercise a particle of faith, yea, even if ye can no more than desire to believe, let this desire work in you, even until ye believe in a manner that ye can give place for a portion of my words."></textarea>
+       <textarea name="chapter" style="width: 600px;" placeholder="But behold, if ye will awake and arouse your faculties, even to an experiment upon my words, and exercise a particle of faith, yea, even if ye can no more than desire to believe, let this desire work in you, even until ye believe in a manner that ye can give place for a portion of my words."></textarea>
     </div>
     <?php 
         foreach ($db->query("SELECT name, id FROM topic") as $row) {
