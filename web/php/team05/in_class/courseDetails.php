b23f6cb7aa9706bb1ee85b00a995e7a802529600
Eli Andrew <and16088@byui.edu>
2018-05-31 16:19:58 -0600

    dsf

--- web/php/team05/in_class/courseDetails.php
+++ web/php/team05/in_class/courseDetails.php
@@ -2,6 +2,28 @@
   require("db_connect.php");
 
   foreach ($_GET as $var) {
+    $courseId = htmlspecialchars($var);
     echo "$var<br>";
   }
+
+  if (isset($db)) {
+    $query = 'SELECT name, "number" FROM course WHERE id=:id';
+    $stmt = $db->prepare($query);
+    $stmt->execute(array(':id' => $courseId));
+    $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);    
+  }
+  else {
+    echo "Database variable not set<br>";
+  }
 ?>
+
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+   <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0">
+   <title><?php echo $rows["name"];  ?></title> 
+</head>
+<body>
+
+</body>
+</html>
