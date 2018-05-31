69cdefe0c8b82c9beb561ab20c61ef36721a436a
Eli Andrew <and16088@byui.edu>
2018-05-31 15:36:18 -0600

    added in class activity

---
+++ web/php/team05/courses.php
@@ -0,0 +1,22 @@
+<?php 
+  
+?>
+
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+   <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0">
+   <title></title> 
+</head>
+<body>
+  <h1>Courses
+
+  <ul>
+    <li><p>Course 1</p></li>
+    <li><p>Course 2</p></li>
+    <li><p>Course 3</p></li>
+    <li><p>Course 4</p></li>
+  </ul>
+
+</body>
+</html>
