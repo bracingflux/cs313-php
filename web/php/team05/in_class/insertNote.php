ee8a38e3c67c1071cd81791cc44fa222bc2526a8
Eli Andrew <and16088@byui.edu>
2018-05-31 16:45:30 -0600

    dsf

--- web/php/team05/in_class/insertNote.php
+++ web/php/team05/in_class/insertNote.php
@@ -1,7 +1,17 @@
-<?php 
+<?php
+  require("db_connect.php");   
   $courseId = htmlspecialchars($_POST["courseId"]);
   $date = htmlspecialchars($_POST["date"]);
   $content = htmlspecialchars($_POST["content"]);
 
   echo "$courseId $date $content<br>";
+
+  $query = 'INSERT INTO note (course_id, content, "date") VALUES (:courseId, :content, :"date")';
+  $stmt = $db->prepare($query);
+  $stmt = bindValue(":courseId", $courseId, PDO:PARAM_INT);
+  $stmt = bindValue(":content", $content, PDO:PARAM_STR);
+  $stmt = bindValue(":date", $date, PDO:PARAM_STR);
+  $stmt->execute();
+
+  echo "Note has been sent<br>";  
 ?>
