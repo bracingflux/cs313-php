b0a8bf034db313c8d92153bb092210e126de86a2
Eli Andrew <and16088@byui.edu>
2018-05-31 17:03:08 -0600

    dsf

--- web/php/team05/in_class/courseDetails.php
+++ web/php/team05/in_class/courseDetails.php
@@ -1,43 +1,34 @@
 <?php
-  require("db_connect.php");
-
-  foreach ($_GET as $var) {
-    $courseId = htmlspecialchars($var);
-    // echo "$var<br>";
-  }
-
-  $name = "";
-
-  if (isset($db)) {
-    $query = 'SELECT name, "number" FROM course WHERE id=:id';
-    $stmt = $db->prepare($query);
-    // OR $stmt->bindValue(":id", $courseId, PDO:PARAM_INT);
-    $stmt->execute(array(':id' => $courseId));
-    $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    foreach ($rows as $row) {
-        $name = $row["name"];
-    }    
-  }
-  else {
-    echo "Database variable not set<br>";
-  }
+require("db_connect.php");
+$courseId = htmlspecialchars($_GET["course_id"]);
+$db = get_db();
+$query = "SELECT name, number FROM course WHERE id=:id";
+$statement = $db->prepare($query);
+$statement->bindValue(":id", $courseId, PDO::PARAM_INT);
+$statement->execute();
+$row = $statement->fetch();
+$number = $row["number"];
+$name = $row["name"];
 ?>
-
 <!DOCTYPE html>
-<html lang="en-US">
+<html>
 <head>
-   <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0">
-   <title><?php echo "$name";  ?></title> 
+  <title></title>
 </head>
 <body>
-  <?php 
-    echo "<h1>Showing notes for " . $name . "</h1>";
-   ?>
-   <form action="insertNote.php" method="POST">
-     <input type="date" name="date"><br>     
-     <textarea name="content" placeholder="Enter note here"></textarea>
-     <input type="hidden" value="<?php echo $courseId; ?>" name="courseId">
-     <button type="submit">Submit Note</button>
-   </form>
+
+<?php
+  echo "<h1>Showing notes for: $number - $name</h1>";
+?>
+
+<form action="insertNote.php" method="POST">
+<input type="hidden" name="course_id" value="<?php echo $courseId; ?>">
+<input type="date" name="date"><br>
+<textarea name="content" placeholder="Content"></textarea>
+
+<br><br>
+<input type="submit" value="Add Note">
+</form>
+
 </body>
 </html>
