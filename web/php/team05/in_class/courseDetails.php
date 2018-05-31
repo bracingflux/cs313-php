cb68baaa3c8ecf5322f22ec0e8aa697e129f82d3
Eli Andrew <and16088@byui.edu>
2018-05-31 16:13:29 -0600

    dsf

--- web/php/team05/in_class/courseDetails.php
+++ web/php/team05/in_class/courseDetails.php
@@ -1,36 +1,7 @@
-<?php 
-	require("db_connect.php");
-
-  if (isset($db)) {
-    $query = 'SELECT id, name, "number" FROM course';
-    $stmt = $db->prepare($query);
-    $stmt->execute();
-    $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);    
+<?php
+  require("db_connect.php");
+   
+  foreach ($_POST as $var) {
+    echo "$var<br>";
   }
-  else {
-    echo "Database variable not set<br>";
-  }
-  
 ?>
-
-<!DOCTYPE html>
-<html lang="en-US">
-<head>
-   <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0">
-   <title></title> 
-</head>
-<body>
-  <h1>Courses</h1>
-
-  <ul>
-    <?php 
-        foreach ($rows as $row) {
-          $name = $row["name"];
-          $number = $row["number"];  
-          echo"<a href='courseDetails.php'></a><li>$number - $name</li>";
-        }
-     ?>
-  </ul>
-
-</body>
-</html>
