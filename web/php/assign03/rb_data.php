d17241e069e303873f6d48a6be784ea698340d61
Eli Andrew <and16088@byui.edu>
2018-06-01 13:04:16 -0600

    more changes to comments

--- web/php/assign03/rb_data.php
+++ web/php/assign03/rb_data.php
@@ -16,7 +16,7 @@
 			. "\n\nBrand: " . $row['brand'];
 			$id = $row['id'];
 		}
-
+/*
 		$stmt2 = $db->prepare('SELECT "text", "timestamp" FROM comments WHERE root_beer_id=:id');
 		$stmt2->execute(array(':id' => $id));
 		$comment_rows = $stmt2->fetchAll(PDO::FETCH_ASSOC);
@@ -26,6 +26,20 @@
 			$splitTime = explode(" ", $time);
 			$time = $splitTime[0];
 			echo  "\"" . $row['text'] . "\" " . $time . "\n\n";
+		}*/
+
+		$stmt = $db->prepare('SELECT c.text, c.timestamp, u.display_name FROM comments c INNER JOIN root_beers rb ON c.root_beer_id = rb.id INNER JOIN users u ON c.user_id = u.id WHERE rb.id =:id');
+		$stmt->execute(array(':id' => $id));
+		$comment_rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+		if (count($comment_rows) > 0) {
+			echo "\n\nComments:\n\n";
+		}
+
+		foreach ($comment_rows as $row) {
+			$time = $row['timestamp'];
+			$splitTime = explode(" ", $time);
+			$time = $splitTime[0];
+			echo  "\"" . $row['text'] . "\" " . $time . "\n\n- " . $row['display_name'] . "\n\n";
 		}
 	}
 	else {
