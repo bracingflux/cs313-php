90c7a22df85751f727c9812ef899994c045ff9cf
Eli Andrew <and16088@byui.edu>
2018-06-01 15:40:23 -0600

    more changes

--- web/php/assign03/a3.php
+++ web/php/assign03/a3.php
@@ -1,3 +1,6 @@
+<?php  
+	session_start();
+?>
 <!DOCTYPE html>
 <html lang="en-US">
 <head>
@@ -16,6 +19,7 @@
 	<a href="https://www.facebook.com/Root-Beer-Revelry-181821778540286/" target="_blank" class="fa fa-facebook" id="shadow"></a>
 	<a href="https://twitter.com/rootbeerrevelry?lang=en" class="fa fa-twitter" target="_blank" id="shadow"></a>	
 	<div id="banner">
+		<p id="current_user">User</p>
 		<img src="../../photos/revelry.jpg" class="center" alt="Root Beer Revelry banner">
 		<button class="log-click" onclick="document.getElementById('id01').style.display='block'">Login</button>						
 		<p class="def">rev·el·ry \'re-vəl-rē\ • n. pl. rev·el·ries • Boisterous merrymaking.</p>
