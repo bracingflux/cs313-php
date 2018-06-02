a10cf00608403fb2c2ce33d68d23e9933a97cae1
Eli Andrew <and16088@byui.edu>
2018-06-01 18:36:06 -0600

    more changes

--- web/php/assign03/submit_comment.php
+++ web/php/assign03/submit_comment.php
@@ -8,9 +8,10 @@
 		$user_id = htmlspecialchars($_POST["userId"]);
 
 		$timestamp = time();
-		$dt = new DateTime("now", new DateTimeZone('America/Denver')); //first argument "must" be a string
-		$dt->setTimestamp($timestamp); //adjust the object to correct timestamp
-		$timestamp = $dt->format('d.m.Y, H:i:s');
+		echo "$timestamp";
+		/*$dt = new DateTime("now", new DateTimeZone('America/Denver'));
+		$dt->setTimestamp($timestamp);
+		$timestamp = $dt->format('d.m.Y, H:i:s');*/
 		try {
 			$query = "INSERT INTO comments (text, timestamp, user_id, root_beer_id) VALUES (:text, :timestamp, :user_id, :root_beer_id)";
 			$statement = $db->prepare($query);
