c6e26f3e60a2587d4eefde26821ad4c7290f7c2b
Eli Andrew <and16088@byui.edu>
2018-06-01 23:14:35 -0600

    another change

--- web/php/assign03/submit_comment.php
+++ web/php/assign03/submit_comment.php
@@ -19,14 +19,19 @@
 			$statement->bindValue(":user_id", $user_id, PDO::PARAM_INT);
 			$statement->bindValue(":root_beer_id", $root_beer_id, PDO::PARAM_INT);		
 			$statement->execute();
-			echo "Comment submitted! ";
+			// echo "Comment submitted! ";
 
 			$newId = $db->lastInsertId();
-			echo $newId;
-			/*$stmt = $db->prepare('SELECT c.text, c.timestamp, u.display_name, u.id FROM comments c INNER JOIN root_beers rb ON c.root_beer_id = rb.id 
-				INNER JOIN users u ON c.user_id = u.id WHERE rb.id =:id AND u.id =:user_id');
-			$stmt->execute(array(':id' => $root_beer_id, ':user_id' => $user_id));
-			$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);	*/
+			// echo $newId;
+			$stmt = $db->prepare('SELECT c.text, c.timestamp, u.display_name, u.id FROM comments c INNER JOIN root_beers rb ON c.root_beer_id = rb.id 
+				INNER JOIN users u ON c.user_id = u.id WHERE c.id =:id');
+			$stmt->execute(array(':id' => $newId));
+			$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+			foreach ($rows as $row) {
+					$time = strtotime($row['timestamp']);
+					echo  "<div class='container_message darker'><p>" . $row['text'] . "\n\n<span style='float: right;'>-" . $row['display_name'] . "</span><span class='time-left'>" . date("h:i A", $time) . "</span>" . "</p></div>";
+				}	
 		}
 		catch(Exception $ex) {
 			echo "There was an error.";
