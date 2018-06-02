e2894881544c49276d6e4c67fdca06ff90f95745
Eli Andrew <and16088@byui.edu>
2018-06-01 18:04:48 -0600

    more changes

--- web/php/assign03/rb_data.php
+++ web/php/assign03/rb_data.php
@@ -23,27 +23,28 @@
 		$stmt = $db->prepare('SELECT c.text, c.timestamp, u.display_name, u.id FROM comments c INNER JOIN root_beers rb ON c.root_beer_id = rb.id INNER JOIN users u ON c.user_id = u.id WHERE rb.id =:id');
 		$stmt->execute(array(':id' => $id));
 		$comment_rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+		if (isset($_SESSION["userId"])) {
+			$saved_id = $_SESSION["userId"];
+			echo "<form class='commentSubmit'>";
+			echo "<textarea name='comment1' class='comment_box' placeholder='Enter your comment here'></textarea>";
+			echo "<button type='submit'>Send Comment</button>";
+			echo "</form>";
+		}
+
 		if (count($comment_rows) > 0) {
 			echo "<p>Comments:\n</p>";
-			if (isset($_SESSION["userId"])) {
-				$saved_id = $_SESSION["userId"];
-				echo "<form class='commentSubmit'>";
-				echo "<textarea name='comment1' class='comment_box' placeholder='Enter your comment here'></textarea>";
-				echo "<button type='submit'>Send Comment</button>";
-				echo "</form>";
-			}
-		}
-
-		foreach ($comment_rows as $row) {			
-			$time = strtotime($row['timestamp']);
-			if ($saved_id == $row['id']) {
-				echo  "<div class='container_message darker'><p>" . $row['text'] . "\n\n<span style='float: right;'>-" . $row['display_name'] . "</span><span class='time-left'>" . date("h:i A", $time) . "</span>" . "</p></div>";
-
-			}
-			else {
-				echo  "<div class='container_message'><p>" . $row['text'] . "\n\n-" . $row['display_name'] . "<span class='time-right'>" . date("h:i A", $time) . "</span>" . "</p></div>";
+			foreach ($comment_rows as $row) {			
+				$time = strtotime($row['timestamp']);
+				if ($saved_id == $row['id']) {
+					echo  "<div class='container_message darker'><p>" . $row['text'] . "\n\n<span style='float: right;'>-" . $row['display_name'] . "</span><span class='time-left'>" . date("h:i A", $time) . "</span>" . "</p></div>";
+				}
+				else {
+					echo  "<div class='container_message'><p>" . $row['text'] . "\n\n-" . $row['display_name'] . "<span class='time-right'>" . date("h:i A", $time) . "</span>" . "</p></div>";
+				}			
 			}			
 		}
+
+		
 	}
 	else {
 		echo "POST not set..";
