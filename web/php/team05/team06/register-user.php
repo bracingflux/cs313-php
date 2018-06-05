73370e2198316593a92345266915d22dfab8a3e7
Eli Andrew <and16088@byui.edu>
2018-06-05 16:36:07 -0600

    more changes

---
+++ web/php/team05/team06/register-user.php
@@ -0,0 +1,51 @@
+<?php 
+	require('db_info.php');
+	session_start();
+
+	$userName = htmlspecialchars($_POST["uName"]);
+
+	$password = htmlspecialchars($_POST["psw"]);
+
+	$rptPassword = htmlspecialchars($_POST["psw-repeat"]);
+
+	if ($password != $rptPassword) {
+	    $_SESSION["message"] = "Passwords do not Match";
+	    header("Location: home.php");
+	}
+
+	$isIN = false;
+
+	//echo $userName . " " . $password;
+
+	 foreach ($db->query('SELECT id, username, password FROM ta07_users') as $row) {
+	    //echo $row["username"] . " " . $row["password"];  
+	   if ($userName == $row["username"] && $password == $row["password"])
+	   {
+	       $_SESSION["messageL"] = "You have an account already. Please login";
+	       header("Location: login.php");
+	       $isIN = true;
+	       break;
+	   }
+	}
+
+	   if(!$isIn)
+
+	   {    
+	       $query = "INSERT INTO ta07_users (username, password) VALUES (:username, :password)";
+	       $statement = $db->prepare($query);
+	       $statement->bindValue(":username", $userName, PDO::PARAM_STR);
+	       $statement->bindValue(":password", $password, PDO::PARAM_STR);
+	       $statement->execute();
+
+	       $_SESSION["user_Name"] = $userName;
+
+
+	       $_SESSION["id"] = $db->lastInsertId('ta07_users_id_seq');
+
+	       echo $_SESSION["id"];
+	       header("Location: welcome.php");
+	       echo "<h1>Done!</h1>";
+	         break;
+	   }
+	   die();
+?>
--- web/php/team05/team06/signUp.php
+++ web/php/team05/team06/signUp.php
@@ -1,4 +1,45 @@
 <?php 
 	require('db_info.php');
-	session_start();	
+	session_start();
 ?>
+
+<!DOCTYPE html>
+<html lang=“en”>
+<head>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width" />
+	<link rel="stylesheet" href="team-css.css" type="text/css" charset="utf-8">
+	<link rel="stylesheet" href="login.css" type="text/css" charset="utf-8">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src="pro.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBu-916DdpKAjTmJNIgngS6HL_kDIKU0aU&callback=myMap"></script>
+
+<title> Data Collection </title>
+</head>
+<body>
+
+ <form action="register-user.php" method="post" style="border:1px solid #ccc">
+
+	<div class="containerSign">
+
+		 <h1>Sign Up</h1>
+		 <p>Please fill in this form to create an account.</p>
+		 <hr>      
+		 <label for=“Username”><b>Username</b></label>
+		 <input type=“text” placeholder=“Enter Username” name=“uName” required><br>		       
+		 <label for=“psw”><b>Password</b></label>
+		 <input type=“password” placeholder=“Enter Password” name=“psw” required><br>      
+		 <label for=“psw-repeat”><b>Repeat Password</b></label>
+		 <input type=“password” placeholder=“Repeat Password” name=“psw-repeat” required><br>
+		 <label>
+		   <input type=“checkbox” checked=“checked” name=“remember” style=“margin-bottom:15px”> Remember me
+		 </label>
+       <button type=“submit” class=“btn btn-primary btn-md signupbtn”>Sign Up</button> 
+	</div>
+
+ </form>
+
+ </body>
+</html>
