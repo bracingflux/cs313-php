0438b4f9a0907e835866f65d7b506795f843b245
Eli Andrew <and16088@byui.edu>
2018-06-05 16:58:34 -0600

    more changes

--- web/php/team05/team06/check-user.php
+++ web/php/team05/team06/check-user.php
@@ -3,24 +3,32 @@
 	session_start();
 
 	$userName = htmlspecialchars($_POST["uName"]);
-
 	$password = htmlspecialchars($_POST["psw"]);
-
-	$rptPassword = htmlspecialchars($_POST["psw-repeat"]);
 	$hashPassword = password_hash($password, PASSWORD_DEFAULT);
 	// echo "Your stuff: $userName $password";
 
-	if ($password != $rptPassword) {
-	    $_SESSION["message"] = "Passwords do not Match";
-	    header("Location: home.php");
-	}
+	
 
 	$isIN = false;
 
 	//echo $userName . " " . $password;
+	try {
+		$query = 'SELECT id, username, password FROM ta07_users WHERE password=:password AND username=:username';
+		$statement = $db->prepare($query);
+		$statement->bindValue(":username", $userName, PDO::PARAM_STR);
+		$statement->bindValue(":password", $hashPassword, PDO::PARAM_STR);
+		$statement->execute();
+		$row = $statement->fetchAll();
 
-	 foreach ($db->query('SELECT id, username, password FROM ta07_users') as $row) {
-	    //echo $row["username"] . " " . $row["password"];  
+		var_dump($row);
+	}
+	catch(Exception $ex) {
+		echo "Invalid credentials";
+	}
+	
+
+	 /*foreach ($db->query('SELECT id, username, password FROM ta07_users WHERE password=:password AND username=:username') as $row) {
+	    echo $row["username"] . " " . $row["password"];  
 	   if ($userName == $row["username"] && $password == $row["password"])
 	   {
 	       $_SESSION["messageL"] = "You have an account already. Please login";
@@ -28,9 +36,9 @@
 	       $isIN = true;
 	       break;
 	   }
-	}
+	}*/
 
-	   if(!$isIn)
+	   /*if(!$isIn)
 
 	   {    
 	       $query = "INSERT INTO ta07_users (username, password) VALUES (:username, :password)";
@@ -48,6 +56,6 @@
 	       header("Location: welcome.php");
 	       echo "<h1>Done!</h1>";
 	         break;
-	   }
+	   }*/
 	   die();
 ?>
