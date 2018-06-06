4aa263dee9d0e2e3c53a29bab03740e5f7937c06
Eli Andrew <and16088@byui.edu>
2018-06-05 21:25:42 -0600

    changes to team07

--- web/php/team05/team06/check-user.php
+++ web/php/team05/team06/check-user.php
@@ -31,37 +31,5 @@
 	catch(Exception $ex) {
 		echo "Failure to connect to database.";
 	}
-	
-
-	 /*foreach ($db->query('SELECT id, username, password FROM ta07_users WHERE password=:password AND username=:username') as $row) {
-	    echo $row["username"] . " " . $row["password"];  
-	   if ($userName == $row["username"] && $password == $row["password"])
-	   {
-	       $_SESSION["messageL"] = "You have an account already. Please login";
-	       header("Location: login.php");
-	       $isIN = true;
-	       break;
-	   }
-	}*/
-
-	   /*if(!$isIn)
-
-	   {    
-	       $query = "INSERT INTO ta07_users (username, password) VALUES (:username, :password)";
-	       $statement = $db->prepare($query);
-	       $statement->bindValue(":username", $userName, PDO::PARAM_STR);
-	       $statement->bindValue(":password", $hashPassword, PDO::PARAM_STR);
-	       $statement->execute();
-
-	       $_SESSION["user_Name"] = $userName;
-
-
-	       $_SESSION["id"] = $db->lastInsertId('ta07_users_id_seq');
-
-	       // echo $_SESSION["id"];
-	       header("Location: welcome.php");
-	       echo "<h1>Done!</h1>";
-	         break;
-	   }*/
 	   die();
 ?>
