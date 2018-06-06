82f79bb8287bf2405b337a5db1d27718a97647cb
Eli Andrew <and16088@byui.edu>
2018-06-05 23:20:36 -0600

    more changes

--- web/php/team05/team06/check-user.php
+++ web/php/team05/team06/check-user.php
@@ -4,13 +4,9 @@
 
 	$userName = htmlspecialchars($_POST["uName"]);
 	$password = htmlspecialchars($_POST["psw"]);
-	// echo "Your stuff: $userName $password";
-
-	
 
 	$isIN = false;
 
-	//echo $userName . " " . $password;
 	try {
 		$query = 'SELECT id, username, password FROM ta07_users WHERE username=:username';
 		$statement = $db->prepare($query);
@@ -18,10 +14,8 @@
 		$statement->execute();
 		$row = $statement->fetch();
 
-		// var_dump($row);
-
 		if (password_verify($password, $row['password'])) {
-			echo "<h1>Welcome " . $row['username'] . "!</h1>";					
+	       $_SESSION["user_Name"] = $userName;								
 	       	header("Location: welcome.php");			
 		}
 		else {
--- web/php/team05/team06/welcome.php
+++ web/php/team05/team06/welcome.php
@@ -8,16 +8,9 @@
 <html lang="en">
 
 <head>
-
-<title>
-
-    Profile
-
-</title>
-
- <?php include('headerA.php');?>
-
-
+	<title>Welcome!</title>
+</head>
+<body>
  <?php 
  	if (isset($_SESSION['user_Name'])) {
  		echo "<h1>Welcome ". $_SESSION['user_Name'] . "!</h1>";
@@ -26,30 +19,5 @@
 	       header("Location: signIn.php");			 		
  	} 
  ?>
-
-
- <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post" class="dataForm" >
-
-   <div class="containerSign">
-
-     <!-- <h1>Search database</h1> -->
-
-     <!-- <p>Please fill in this form to search for information.</p> -->
-
-   
-
-
-
-     <div class="clearfix">
-
-       <!-- <button type="button" class="btn btn-primary btn-md cancelbtn">Cancel</button> -->
-
-       <button type="submit" class="btn btn-primary btn-md signupbtn">Search</button>  
-
-     
-
-     </div>
-
-   </div>
-
- </form>
+ </body>
+</html>
