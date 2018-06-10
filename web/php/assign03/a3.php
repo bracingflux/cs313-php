af0294a4f24458a3609830c370ac267543af1fd6
Eli Andrew <and16088@byui.edu>
2018-06-09 21:42:05 -0600

    more changes

--- web/php/assign03/a3.php
+++ web/php/assign03/a3.php
@@ -10,9 +10,7 @@
 	<link rel="stylesheet" href="/resources/demos/style.css">
 	<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
 	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>	
+		
 	<!-- <script type="text/javascript" src="../../javascript/a3.js"></script> -->
 	<link rel="stylesheet" type="text/css" href="../../css/a3.css">
 	<title>Root Beer Revelry</title>	
