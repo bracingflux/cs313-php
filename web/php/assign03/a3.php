7a4761d80b452afafb91f61731738f2f0a6ce00a
Eli Andrew <and16088@byui.edu>
2018-06-12 13:17:34 -0600

    more changes

--- web/php/assign03/a3.php
+++ web/php/assign03/a3.php
@@ -127,10 +127,10 @@
 
 	foreach ($db->query('SELECT name FROM root_beers') as $row) {
 		echo "<div class='rbItem'><img src=\"../../photos/rbs/" . $row['name'] . ".png\" id='rbPhoto' alt='" . $row['name'] . "'>";
-		echo "<p id='rbName'>" . $row['name'] ."</p>";
+		echo "<div id='div1'><p id='rbName'>" . $row['name'] ."</p>";
 		echo "<form class='rbForm'>";
 		echo "<button type='submit' class='view_rb'>More info</button>";
-		echo "<input type='hidden' class='hidden1' name='rb' value=\"" . $row['name'] . "\"></form></div>";
+		echo "<input type='hidden' class='hidden1' name='rb' value=\"" . $row['name'] . "\"></form></div></div>";
 	}		
 	?>
 </body>
