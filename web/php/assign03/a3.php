456ac294cd1dcc1f38f9e3b8e8d6fcced67a9ed4
Eli Andrew <and16088@byui.edu>
2018-06-12 09:53:00 -0600

    more changes

--- web/php/assign03/a3.php
+++ web/php/assign03/a3.php
@@ -28,7 +28,7 @@
 	<title>Root Beer Revelry</title>	
 </head>
 <body>
-<span style="float: right; width: 6%; background: orange; margin: 0; padding: 0;">
+<span style="float: right; width: 9%; background: orange; margin: 0; padding: 0;">
 	<a href="https://www.facebook.com/Root-Beer-Revelry-181821778540286/" target="_blank" class="fa fa-facebook" id="shadow"></a>
 	<a href="https://twitter.com/rootbeerrevelry?lang=en" class="fa fa-twitter" target="_blank" id="shadow"></a>
 </span>
