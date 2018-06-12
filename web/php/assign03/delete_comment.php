abb1908a2533a39937c43cb4a1158d4961d0633c
Eli Andrew <and16088@byui.edu>
2018-06-11 22:35:13 -0600

    more changes

--- web/php/assign03/delete_comment.php
+++ web/php/assign03/delete_comment.php
@@ -1,7 +1,18 @@
 <?php  
 	if (isset($_POST)) {
 		$id = $_POST['comment'];
-		echo "This is the comment id: $id";		
+		echo "This is the comment id: $id";
+		try {
+			$query = "DELETE FROM comments WHERE id=:id";
+			$statement = $db->prepare($query);
+			$statement->bindValue(":id", $id);
+			$statement->execute();
+			echo "Success!";				
+		}
+		catch(Exception $ex) {
+			echo "Failure";
+		} 
+			
 	}
 	else {
 		echo "Post not set.";
